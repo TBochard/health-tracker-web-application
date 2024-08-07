9923d6dbdb1e43bc3c68b5bc7783856ce2468bfa
TBochard <tim.bochard@astrazeneca.com>
2024-08-07 21:06:56 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,7 @@
 module.exports = {
-  // Use jsdom environment for DOM-related testing
   testEnvironment: 'jsdom',
-
-  // Specify the root directory for your tests
+  testPathIgnorePatterns: ['/node_modules/']
   roots: ['<rootDir>/tests'],
-
-  // Automatically clear mock calls and instances between every test
   clearMocks: true,
-
-  // Enable verbose output for more detailed test results
   verbose: true,
 };
